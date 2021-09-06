880080b5b7b8c49cee108c60bcfb4d57c97675a5
Bayurzx <bayurzx@gmail.com>
2021-09-06 07:35:17 +0100

    done

--- circle.py
+++ circle.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-  
+
 # Read image.
 img = cv2.imread('images.jfif', cv2.IMREAD_COLOR)
-  
+
 # Convert to grayscale.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  
-# Blur using 3 * 3 kernel.
+
+# Blur
 gray_blurred = cv2.blur(gray, (4, 4))
 
 detected_circles = cv2.HoughCircles(gray,
@@ -19,16 +19,16 @@ print(f'The number of pipes are: {len(detected_circles[0])}')
 # Draw circles that are detected.
 if detected_circles is not None:
 
-    # Convert the circle parameters a, b and r to integers.
+    # Detect the parameters a, b and r.
     detected_circles = np.uint16(np.around(detected_circles))
 
     for pt in detected_circles[0, :]:
         a, b, r = pt[0], pt[1], pt[2]
 
-        # Draw the circumference of the circle.
+        # Draw the circle.
         cv2.circle(img, (a, b), r, (0, 255, 0), 2)
 
-        # Draw a small circle (of radius 1) to show the center.
+        # Add a dot to circle.
         cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
         cv2.imshow("Detected Circle", img)
         cv2.waitKey(0)
